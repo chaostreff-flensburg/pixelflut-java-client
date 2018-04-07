e0fe64019019af226a2691a448e26f518e3b0e10
Stefan Erichsen <stefan@balticfinance.com>
2018-04-07 20:46:51 +0200

    added scripts to compile and run

---
+++ compile.sh
@@ -0,0 +1,2 @@
+mkdir -p out/production/pixelflut/
+javac src/de/twimbee/* -d out/production/pixelflut/
---
+++ run.sh
@@ -0,0 +1,2 @@
+cd out/production/pixelflut/
+java de.twimbee.Main 94.45.224.9 1337 300 440 /home/stefan/Downloads/raked-rechts.png
