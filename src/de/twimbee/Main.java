c936cef7c8ba6e267ac9d746ee139aabcdec9616
Stefan Erichsen <stefan@balticfinance.com>
2018-04-09 14:26:24 +0200

    added default config for running

--- src/de/twimbee/Main.java
+++ src/de/twimbee/Main.java
@@ -13,27 +13,33 @@ public class Main {
             System.out.println("java de.twimbee.Main Host Port PositionX PositionY ImagePath");
             System.out.println("Example:");
             System.out.println("java de.twimbee.Main 94.45.224.9 1337 500 230 /home/stefan/Downloads/raked-half.png");
+            System.out.println("Now running default config :-)");
+            paintImage("94.45.224.9", 1337, 0, 0, "/home/stefan/Downloads/raked-half.png");
         } else {
             String host = args[0];
             int port = Integer.parseInt(args[1]);
             int positionX = Integer.parseInt(args[2]);
             int positionY = Integer.parseInt(args[3]);
 
-            BufferedImage image = ImageIO.read(new File(args[4]));
-            int width = image.getWidth();
-            int height = image.getHeight();
+            paintImage(host, port, positionX, positionY, args[4]);
+        }
+    }
 
-            int threads = Runtime.getRuntime().availableProcessors();
-            System.out.println("Using " + threads + " threads");
+    private static void paintImage(String host, int port, int positionX, int positionY, String imageUrl) throws IOException {
+        BufferedImage image = ImageIO.read(new File(imageUrl));
+        int width = image.getWidth();
+        int height = image.getHeight();
 
-            int widthPerThread = width / threads;
+        int threads = Runtime.getRuntime().availableProcessors();
+        System.out.println("Using " + threads + " threads");
 
-            for (int t = 0; t < threads; t++) {
-                WriterThread writerThread = new WriterThread(host, port, positionX, positionY, t * widthPerThread, ((t + 1) * widthPerThread) - 1, 0, height, image);
-                Thread thread = new Thread(writerThread, "Thread #" + (t + 1));
-                thread.start();
-                System.out.println(thread.getName() + " started");
-            }
+        int widthPerThread = width / threads;
+
+        for (int t = 0; t < threads; t++) {
+            WriterThread writerThread = new WriterThread(host, port, positionX, positionY, t * widthPerThread, (t + 1) * widthPerThread, 0, height, image);
+            Thread thread = new Thread(writerThread, "Thread #" + (t + 1));
+            thread.start();
+            System.out.println(thread.getName() + " started");
         }
     }
 }
